f92fc2fa7ebd24b954af113a4184772246879bce
Manuel Meneses <hbiaser132@gmail.com>
2025-03-03 20:25:12 -0400

    Agregar pruebas de extremo a extremo para la API de Express, incluir supertest y ajustar la exportación de la aplicación

---
+++ src/index.spec.ts
@@ -0,0 +1,137 @@
+import request from "supertest";
+import { describe, it, expect } from "vitest";
+
+describe("End-to-End tests for Express API", () => {
+  beforeAll(() => {
+    global.fetch = vi.fn().mockResolvedValue({
+      ok: true,
+      async json() {
+        return {
+          $schema: "./mock.schema.json",
+          mocks: [
+            {
+              method: "POST",
+              path: "/person",
+              responses: [
+                {
+                  in: "body",
+                  where: {
+                    id: 1,
+                  },
+                  response: {
+                    id: "1",
+                    name: "Jhon Doe",
+                    age: "33",
+                  },
+                },
+                {
+                  in: "body",
+                  where: {
+                    id: 2,
+                  },
+                  response: {
+                    id: "2",
+                    name: "Jane Doe",
+                    age: "28",
+                  },
+                },
+                {
+                  response: {
+                    message:
+                      "Respuesta por defecto, no se encontró coincidencia con 'where'",
+                  },
+                },
+              ],
+            },
+            {
+              method: "GET",
+              path: "/videos/:id",
+              responses: [
+                {
+                  in: "param",
+                  where: {
+                    id: 1,
+                  },
+                  response: {
+                    id: 1,
+                    name: "Mock testing",
+                  },
+                },
+                {
+                  in: "param",
+                  where: {
+                    id: 2,
+                  },
+                  response: {
+                    id: 2,
+                    name: "Playwrite testing",
+                  },
+                },
+              ],
+            },
+          ],
+        };
+      },
+    });
+  });
+
+  const tests = [
+    {
+      method: "get",
+      path: "/skapxd/bc-mocks-apis/videos/1",
+      expected: {
+        id: 1,
+        name: "Mock testing",
+      },
+    },
+    {
+      method: "get",
+      path: "/skapxd/bc-mocks-apis/videos/2",
+      expected: {
+        id: 2,
+        name: "Playwrite testing",
+      },
+    },
+    {
+      method: "post",
+      path: "/skapxd/bc-mocks-apis/person",
+      body: {
+        id: 1,
+      },
+      expected: {
+        id: "1",
+        name: "Jhon Doe",
+        age: "33",
+      },
+    },
+    {
+      method: "post",
+      path: "/skapxd/bc-mocks-apis/person",
+      body: {
+        id: 2,
+      },
+      expected: {
+        id: "2",
+        name: "Jane Doe",
+        age: "28",
+      },
+    },
+  ];
+
+  it.each(tests)(
+    "Ejecutando el test $method $path $body",
+    async ({ path, expected, method, body }) => {
+      const { app } = await import(".");
+
+      if (method === "get") {
+        const response = await request(app).get(path);
+        expect(response.body).toEqual(expected);
+      }
+
+      if (method === "post") {
+        const response = await request(app).post(path).send(body);
+        expect(response.body).toEqual(expected);
+      }
+    }
+  );
+});
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import "source-map-support/register";
 import express from "express";
 import { mockController } from "./controllers";
 
-const app = express();
+export const app = express();
 // Middleware para parsear el JSON del body
 app.use(express.json());
 
 // Se delega todas las rutas al controlador de mocks
 app.all("*", async (req, res, next) => {
   try {
-    await mockController(req, res, next);
+    await mockController(req, res);
   } catch (error) {
     next(error);
   }
@@ -18,10 +18,12 @@ app.all("*", async (req, res, next) => {
 
 // Middleware de manejo de errores
 app.use((err, req, res, next) => {
-  console.error(err.stack);
+  console.error(err);
   res.status(500).json({ message: "Algo salió mal!", error: err.message });
 });
 
-app.listen(process.env.PORT || 3000, () =>
-  console.log("Server ready on port 3000.")
-);
+if (require.main === module) {
+  app.listen(process.env.PORT || 3000, () =>
+    console.log("Server ready on port 3000.")
+  );
+}
